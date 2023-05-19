c5ef77d5917319338aa9286b7b47690cf41aa4a8
UhOhSpiders <iain.mccall.95@gmail.com>
2023-05-19 12:38:25 +0100

    add remove cash

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -5,7 +5,15 @@ def get_pet_shop_name(pet_shop):
 def get_total_cash(pet_shop):
     return pet_shop["admin"]["total_cash"]
 
+def add_or_remove_cash(pet_shop, cash_int):
+    pet_shop["admin"]["total_cash"] =+ cash_int
+    return 
+
+
+
+
 def find_pet_by_name(pet_shop, pet_name):
     for pet in pet_shop["pets"]:
         if pet["name"]== pet_name:
             return pet
+
