cc262627bcaeff745d34e313c5cc6fabbcb2e9b4
UhOhSpiders <iain.mccall.95@gmail.com>
2023-05-19 13:18:22 +0100

    stock count & get pets by breed

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -16,6 +16,19 @@ def increase_pets_sold(pet_shop, sale_int):
     pet_shop["admin"]["pets_sold"] += sale_int
     return
 
+def get_stock_count(pet_shop):
+    return len(pet_shop["pets"])
+
+def get_pets_by_breed(pet_shop, breed_name):
+    pets = []
+    for pet in pet_shop["pets"]:
+        if pet["breed"] == breed_name:
+            pets.append(1)
+    return pets
+
+
+
+
 def find_pet_by_name(pet_shop, pet_name):
     for pet in pet_shop["pets"]:
         if pet["name"]== pet_name:
