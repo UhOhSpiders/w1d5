fed655489947f280e771d2e7194ef78292b1cb08
UhOhSpiders <iain.mccall.95@gmail.com>
2023-05-20 09:31:25 +0100

    pet count, add new pet

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -44,5 +44,9 @@ def get_customer_cash(customer):
 def remove_customer_cash(customer, cash_int):
     customer["cash"] -= cash_int
 
+def get_customer_pet_count(customer):
+    return len(customer["pets"])
 
+def add_pet_to_customer(customer, new_pet):
+    customer["pets"].append(new_pet)
     
